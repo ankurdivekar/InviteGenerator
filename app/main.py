bb38c64e3cb3dac4c91372cdb35f0e48a1618fbe
Ankur Divekar <ankur.divekar@gmail.com>
2023-02-04 23:44:43 +0530

    First commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import io
+import qrcode
+
+from fastapi import FastAPI
+from starlette.responses import StreamingResponse
+
+app = FastAPI()
+
+
+@app.get("/generate/{message}")
+def generate(message: str):
+    img = qrcode.make(message)
+    buf = io.BytesIO()
+    img.save(buf)
+    buf.seek(0)  # important here!
+    return StreamingResponse(buf, media_type="image/jpeg")
